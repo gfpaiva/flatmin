ab0fb688308355e6dfb4ec950c65a6c93049631e
Lucas Leandro <lucasleandro1204@gmail.com>
2017-08-30 03:34:29 -0300

    switch box

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
 Vue.component('ProgressBar', {
-  template: `
-    <div class="progress-bar">
-      <div class="progress-bar--stage" :style="{ width }">
-        <div class="progress-bar--percent">{{ percent }}</div>
-      </div>
-    </div>
-  `,
-
   props: {
     percent: {
       type: Number,
@@ -17,11 +9,50 @@ Vue.component('ProgressBar', {
     }
   },
 
+  template: `
+    <div class="progress-bar">
+      <div class="progress-bar--stage" :style="{ width }">
+        <div class="progress-bar--percent">{{ percent }}</div>
+      </div>
+    </div>
+  `,
+
   computed: {
     width () {
       let size = this.percent <= 50 ? '+ 60px' : '';
 
       return `calc(${this.percent}% ${size})`;
+    }
+  }
+});
+
+Vue.component('SwitchBox', {
+  props: {
+    value: {
+      type: Boolean,
+      default: true,
+    }
+  },
+
+  data () {
+    return {
+      checked: this.value,
+    }
+  },
+
+  template: `
+    <div class="switch-box">
+      <label class="switch">
+      <input type="checkbox" class="switch--chekbox" @click="toggle" :checked="checked">
+      <span class="switch--slider switch--round"></span>
+      </label>
+    </div>
+  `,
+
+  methods: {
+    toggle () {
+      this.checked = !this.checked;
+      this.$emit('input', this.checked);
     }
   }
 });
